4a9685128295871d7b01b9a8e6f6d84d53668db8
hunter-aw <hunteraweinsheink@gmail.com>
2018-10-21 16:14:06 +0300

    redit scarper

---
+++ reddit.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const url = 'https://www.reddit.com';
+
+puppeteer
+    .launch()
+    .then((browser) => {
+        return browser.newPage();
+    })
+    .then((page) => {
+        return page.goto(url).then(() => {
+            return page.content();
+        });
+    })
+    .then((html) => {
+        $('h2', html).each(function() {
+            console.log($(this).text());
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
